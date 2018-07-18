c12978d338916b8207a25d2120416838204729b3
prasath <prasath1196@gmail.com>
2018-07-18 11:53:58 +0530

    React Learning -Udemy course basics completed

---
+++ BookList/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+	{title : 'Javascript', pages:101},
+	{title : 'Harry Potter', pages:101},
+	{title : 'Rich dad', pages:101},
+	{title : 'Think and Grow Rich',pages:101}
+	]
+}
---
+++ blog/src/components/posts_new.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import {connect } from 'react-redux';
+import { createPost } from '../actions'
+
+class PostsNew extends Component {
+
+  renderField(field) {
+    { touched }
+    const { meta: { touched, error } } = field;
+    const className =`form-group ${ touched && error ? 'has-danger' : '' }`
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input className ="form-control"
+               type = "text"
+        //three dots is a syntax for passing all the onchange,
+        //onfocus properties of the field to input tag
+          {...field.input}
+        />
+        <div className ="text-help">
+          {touched? error : ''}
+        </div>
+      </div>
+      );
+  }
+
+  onSubmit(values){
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render(){
+   const { handleSubmit } = this.props;
+  return (
+    <form onSubmit = { handleSubmit(this.onSubmit.bind(this)) } >
+      <Field
+        label="Title for Post"
+        name = "title"
+        component = { this.renderField }
+      />
+       <Field
+          label="Categories"
+          name="categories"
+          component={ this.renderField }
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={ this.renderField }
+        />
+        <button type ="submit" className="btn btn-primary"> Submit </button>
+        <Link className="btn btn-danger" to="/">
+          Cancel
+        </Link>
+    </form>
+    );
+  }
+}
+
+function validate (values) {
+  const errors = {};
+
+  if( !values.title ) {
+    errors.title = "Enter a title!";
+  }
+
+  if (!values.categories ){
+    errors.categories = "Enter some categories";
+  }
+
+  if(!values.content) {
+    errors.content = 'Enter some Content';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+}) (
+    connect(null,{ createPost} )(PostsNew)
+);
+
+
